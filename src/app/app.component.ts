90f219008b8bc0297e191018698e29898aab349c
lokeshwebdeve@gmail.com <lokeshwebdeve@gmail.com>
2021-07-31 15:13:17 +0530

    first done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+
+todolist:any[] = [];
+ 
+ todolistFuncetion(todoval:any)
+
+{
+  this.todolist.push(todoval.value);
+
+  todoval.value='';  
+} 
+removeTodoitem(item:any) {
+  this.todolist.splice(item,1);
+}
+
+itemname="";
+indexval:any="";
+displayval=false;
+displayval1=true;
+updateItem(item:any){ 
+
+  this.displayval=true;
+  this.displayval1=false;
+  this.itemname=item;
+  
+  this.indexval=this.todolist.indexOf(item);
+
+   //index = this.todolist.indexOf(item);
+
+}
+edittodo(todoitem:any,todoitemindex:any)
+{   
+  this.displayval1=true;
+  this.displayval=false;
+  //alert(todoitem.value+todoitemindex.value);
+  this.todolist[todoitemindex.value] = todoitem.value;
+}
+
 }
